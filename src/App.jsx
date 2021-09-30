b310dfb3afffe21c2b1a054017f927d9a311c292
grabovszky <gaborkristof@protonmail.com>
2021-09-30 06:38:17 +0200

    refactor(Navbar): cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,26 @@
 import React, { useState } from 'react';
 
-import { Button, Navbar, Container } from 'react-bootstrap';
-
+import Header from './components/Header';
 import SettingsModal from './components/SettingsModal';
 
 function App() {
   const [difficulty, setDifficulty] = useState(null);
-  const [modalShow, setModalShow] = useState(false);
-
-  const getDifficulty = (data) => {
-    setDifficulty(data);
-  };
+  const [settingsModalShow, setSettingsModalShow] = useState(false);
 
   return (
     <div className="App">
-      <header>
-        <Navbar bg="dark" className="shadow">
-          <Container>
-            <Navbar.Brand href="#home">
-              <img
-                src="/images/evista.png"
-                width="110"
-                height="38"
-                className="d-inline-block align-top"
-                alt="Evista logo"
-              />
-            </Navbar.Brand>
-
-            {difficulty === null ? (
-              <>
-                <Button variant="primary" onClick={() => setModalShow(true)}>
-                  Start new Game
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button
-                  variant="secondary"
-                  onClick={() => {
-                    const previousDifficulty = difficulty;
-                    setDifficulty(null);
-                    setTimeout(() => {
-                      setDifficulty(previousDifficulty);
-                    }, 5);
-                  }}
-                >
-                  Start over
-                </Button>
-                <Button onClick={() => setDifficulty(null)}>
-                  Start a new Game
-                </Button>
-              </>
-            )}
-          </Container>
-        </Navbar>
-      </header>
-
-      <SettingsModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        getDifficulty={getDifficulty}
-      />
+      <main className="main-content">
+        <Header
+          isplaying={difficulty === null ? false : true}
+          setdifficulty={(e) => setDifficulty(e)}
+          showsettings={(e) => setSettingsModalShow(e)}
+        />
+        <SettingsModal
+          show={settingsModalShow}
+          onHide={() => setSettingsModalShow(false)}
+          setdifficulty={(e) => setDifficulty(e)}
+        />
+      </main>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Button, Container, Navbar } from 'react-bootstrap';
 
-const Header = () => (
-  <header>
+/**
+ * Header navbar component
+ *
+ * @param {boolean} props.isplaying true if the game is in play
+ * @param props.setdifficulty function to reset the difficulty to start a new game
+ * @param props.showsettings function to show the settings modal
+ *
+ * @returns the navbar
+ */
+const Header = (props) => {
+  return (
     <Navbar bg="dark" className="shadow">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="#home" onClick={() => props.setdifficulty(null)}>
           <img
             src="/images/evista.png"
             width="110"
             height="38"
             className="d-inline-block align-top"
-            alt="Evista logo"
+            alt="Evista"
           />
         </Navbar.Brand>
+        {props.isplaying ? (
+          // only show the button if the player is currnetly playing a game
+          <Button
+            variant="primary"
+            className="px-4"
+            onClick={() => {
+              props.setdifficulty(null);
+              props.showsettings(true);
+            }}
+          >
+            Start new Game
+          </Button>
+        ) : null}
       </Container>
     </Navbar>
-  </header>
-);
+  );
+};
 
 export default Header;
