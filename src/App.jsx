7e8bb4a4de53ca97ff28fd90544bb99dcd496337
grabovszky <gaborkristof@protonmail.com>
2021-09-30 06:39:34 +0200

    feat(footer): add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
 
 import Header from './components/Header';
 import SettingsModal from './components/SettingsModal';
@@ -21,6 +22,40 @@ function App() {
           setdifficulty={(e) => setDifficulty(e)}
         />
       </main>
+
+      <Container className="footer">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p className="col-md-4 mb-0 text-muted">© 2021 Kristóf Gábor</p>
+
+          <span className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img
+              alt=""
+              width="50px"
+              height="50px"
+              src={`${process.env.PUBLIC_URL}/images/react.svg`}
+            />
+          </span>
+
+          <ul className="nav col-md-4 justify-content-end">
+            <li className="nav-item">
+              <a
+                href="https://evista.hu/"
+                className="nav-link px-2 text-muted hover-blue"
+              >
+                eVista
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://gaborkristof.com/"
+                className="nav-link px-2 text-muted hover-blue"
+              >
+                gaborkristof
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </Container>
     </div>
   );
 }
